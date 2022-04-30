0b327f0ba2c4fb743dd831518d36a2f91a4c648f
Mar-Nb <niombela.martin@gmail.com>
2022-04-30 11:25:02 +0200

    Mise en place squelette Carte et PNJ

--- src/app/carte/carte.component.ts
+++ src/app/carte/carte.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-carte',
-  templateUrl: './carte.component.html',
-  styleUrls: ['./carte.component.css']
+	selector: 'app-carte',
+	templateUrl: './carte.component.html',
+	styleUrls: ['./carte.component.css']
 })
 export class CarteComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	isScrolling: boolean = false;
+
+	ngOnInit(): void {
+		// Changer le menu-item qui a la classe "is-active"
+		document.querySelectorAll(".menu-list a").forEach((link) => {
+			link.addEventListener("click", () => {
+				const activeLink = document.querySelector(".menu-list a.is-active");
+				activeLink?.classList.remove("is-active");
+				link.classList.add("is-active");
+			});
+		});
+
+		// ScrollSpy du menu - Dès qu'on franchit le milieu horizontal du viewport, on est notifié de l'élément visible
+		const articles = document.getElementsByTagName("article");
+		const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+			for (const entry of entries) {
+				if (entry.isIntersecting && !this.isScrolling) {
+					const itemMenu = document.querySelector(`a[data-id='${entry.target.id}']`);
+					const activeLink = document.querySelector(".menu-list a.is-active");
+					activeLink?.classList.remove("is-active");
+					itemMenu?.classList.add("is-active");
+				}
+			}
+		}, { rootMargin: "-50% 0px" });
+
+		for (let i = 0; i < articles.length; i++) { observer.observe(articles[i]); }
+	}
+
+	scrollTo(element: string): void {
+		this.isScrolling = true;
+		document.getElementById(element)?.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+
+		// Utilisation du setTimeout() pour éviter de voir tous les menu-item passer un à un en "is-active"
+		setTimeout(() => { this.isScrolling = false; }, 900);
+	}
 
 }
--- src/app/pnj/pnj.component.ts
+++ src/app/pnj/pnj.component.ts
@@ -9,7 +9,49 @@ export class PnjComponent implements OnInit {
 
   constructor() { }
 
+  openModal(el: HTMLElement) {
+    el.classList.add("is-active");
+    document.getElementsByTagName("html")[0].classList.add("is-clipped");
+  }
+
+  closeModal(el: HTMLElement) {
+    el.classList.remove("is-active");
+    document.getElementsByTagName("html")[0].classList.remove("is-clipped");
+  }
+
+  closeAllModals() {
+    (document.querySelectorAll(".modal") || []).forEach((modal) => { this.closeModal(modal as HTMLElement); });
+  }
+
   ngOnInit(): void {
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll("[data-trigger='modal']") || []).forEach((btn) => {
+      const modal = (btn as HTMLElement).dataset["target"] ?? "";
+      const $target = document.getElementById(modal);
+      console.log($target);
+
+      btn.addEventListener("click", () => {
+        this.openModal($target as HTMLElement);
+      });
+    });
+
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+      const $target = $close.closest('.modal');
+
+      $close.addEventListener('click', () => {
+        this.closeModal($target as HTMLElement);
+      });
+    });
+
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+      const e = event || window.event;
+
+      if (e.key === "Escape") { // Escape key
+        this.closeAllModals();
+      }
+    });
   }
 
 }
